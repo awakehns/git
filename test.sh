a6c85fe0b27a61a35a6fdbd8eb0aa9c8afacb083
deminge <awake1394hns@gmail.com>
2025-01-12 18:12:35 +0500

    Update test.sh script

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #dev, commit1
 #dev, commit2
 echo 'Hello, this is a test script'/home/awakehns/Netology/sdvps-homeworks/lecture_demos/6-04/lecture_demos/demo1/prometheus.yml'lecture_demos/6-04/lecture_demos/demo1/prometheus.yml
+echo 'This scropt is a test for Git workglows'/home/awakehns/Netology/sdvps-homeworks/lecture_demos/6-04/lecture_demos/demo1/prometheus.yml'lecture_demos/6-04/lecture_demos/demo1/prometheus.yml
